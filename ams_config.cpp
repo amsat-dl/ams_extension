08f3f4789c58ad955c1f99952d9c252205693d1d
Kurt <km@flyheli.de>
2021-05-09 15:57:08 +0200

    initial commit

--- ams_config.cpp
+++ ams_config.cpp
@@ -22,7 +22,7 @@
 
 // config file location
 // enter directory and name of the configuration file
-#define CONFIGFILE		"/home/odroid/ams_config.txt"
+#define CONFIGFILE		"~/ams_config.txt"
 
 // ============= read Beacon Config file ===================
 
